4c269d414e5faf2247c16a1b8f0490f276c8f8df
Clayton Pabst <claytonpabst@gmail.com>
2019-05-13 23:49:12 -0600

    finished

---
+++ splash.js
@@ -0,0 +1,14 @@
+function setCarouselButtonHeight(){
+  const itemHeight = document.getElementsByClassName("carousel-item")[0].children[0].clientHeight
+  const buttons = document.getElementsByClassName("carousel-button")
+  for (let button of buttons) {
+    button.style.lineHeight = itemHeight + "px"
+  }
+}
+
+function windowResized() {
+  setCarouselButtonHeight()
+}
+
+window.onresize = windowResized
+setCarouselButtonHeight()
